36ba88303d3491bd79fe6e460d3cced8d45191a5
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-08-17 13:09:25 -0400

    Initial commit

---
+++ src/main/java/com/ejo/tradecompanion/App.java
@@ -0,0 +1,30 @@
+package com.ejo.tradecompanion;
+
+import com.ejo.glowlib.math.Vector;
+import com.ejo.glowui.Window;
+import com.ejo.tradecompanion.scenes.TitleScene;
+
+public class App {
+
+    //TODO: Have Probability Trading
+    // Have Chart View Menu
+    // Import StockDownloader as a dependency
+    // Have practice mode where you can mark the start of the trade and watch if it goes up or down
+
+    private static Window window = new Window(
+            "Trade Companion",
+            new Vector(100,100),
+            new Vector(1200,800),
+            new TitleScene(),
+            true, 4, 60, 60
+    );
+
+    public static void main(String[] args) {
+        getWindow().run();
+        getWindow().close();
+    }
+
+    public static Window getWindow() {
+        return window;
+    }
+}
---
+++ src/main/java/com/ejo/tradecompanion/scenes/TitleScene.java
@@ -0,0 +1,11 @@
+package com.ejo.tradecompanion.scenes;
+
+import com.ejo.glowui.scene.Scene;
+
+public class TitleScene extends Scene {
+
+    public TitleScene() {
+        super("Title Scene");
+    }
+
+}
