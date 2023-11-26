352c7f6f5bb885bf72d30ebc3d82378c01f20114
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-11-26 12:30:09 -0500

    Made advancements in ProbabilityScene. It is very disorganized and hard to read. My plan is to clean this up soon

--- src/main/java/com/ejo/tradecompanion/scenes/ProbabilityScene.java
+++ src/main/java/com/ejo/tradecompanion/scenes/ProbabilityScene.java
@@ -1,5 +1,6 @@
 package com.ejo.tradecompanion.scenes;
 
+import com.ejo.glowlib.math.MathE;
 import com.ejo.glowlib.math.Vector;
 import com.ejo.glowlib.misc.ColorE;
 import com.ejo.glowlib.time.DateTime;
@@ -61,6 +62,8 @@ public class ProbabilityScene extends Scene {
         super.tick();
     }
 
+    int timeAdd = 0;
+
     @Override
     public void onKeyPress(int key, int scancode, int action, int mods) {
         super.onKeyPress(key, scancode, action, mods);
@@ -71,49 +74,118 @@ public class ProbabilityScene extends Scene {
         }
 
         if (action != Key.ACTION_PRESS) return;
-        if (key == Key.KEY_C.getId()) {
-            dateTimeIDList.clear();
-            //This is the example datapoint candle I am using
-
-            // ----------------------------------------
-            //EDITABLE DATA
-            time = new DateTime(2014,7,14,14,13,0);
-            double maxPercentDiff = (double) 20 /100;
-            boolean doPriceScaling = true;
-            // ----------------------------------------
-
-            float open = stock.getOpen(time);
-            float close = stock.getClose(time);
-            float min = stock.getMin(time);
-            float max = stock.getMax(time);
-            float ocDiff = getOpenCloseDifference(open,close);
-            float topWick = getTopWickSize(max,open,close);
-            float bottomWick = getBottomWickSize(min,open,close);
-
-
-            //TODO: Figure out why candles that exist around new years are RARELY not accurately similar... weird
-            int similarCandleCount = 0;
-            for (Map.Entry<Long, String[]> data : stock.getHistoricalData().entrySet()) { //Loops through all stock data
-                float dataOpen = Float.parseFloat(data.getValue()[0]);
-                float dataClose = Float.parseFloat(data.getValue()[1]);
-                float dataMin = Float.parseFloat(data.getValue()[2]);
-                float dataMax = Float.parseFloat(data.getValue()[3]);
-
-                //This value scales based on the total cap of the stock over history so similarities have a chance
-                float pricingScale = doPriceScaling ? dataOpen / open : 1;
-
-                //TODO: Maybe replace this with "isWithinMargin" where you specify the price margin it has to be within and that can be scaled based off of price scaling
-                boolean bodySize = isWithinDifference(getOpenCloseDifference(dataOpen,dataClose),ocDiff * pricingScale,maxPercentDiff);
-                boolean topWickSize = isMagnitudeWithinDifference(getTopWickSize(dataMax,dataOpen,dataClose),topWick * pricingScale,maxPercentDiff);
-                boolean bottomWickSize = isMagnitudeWithinDifference(getBottomWickSize(dataMin,dataOpen,dataClose),bottomWick * pricingScale,maxPercentDiff);
-
-                if (bodySize && topWickSize && bottomWickSize) {
-                    dateTimeIDList.add(data.getKey());
-                    similarCandleCount ++;
-                }
-            }
-            text.setText(String.valueOf(similarCandleCount));
+        if (key == Key.KEY_H.getId()) {
+            time = new DateTime(2021,7,14,14,13,timeAdd);
+            timeAdd += 60;
         }
+
+        if (key == Key.KEY_C.getId()) {
+            Thread thread = new Thread(() -> {
+                runProbabilityCalculation(stock,time,.02f,true);
+            });
+            thread.setDaemon(true);
+            thread.start();
+        }
+    }
+
+    private void runProbabilityCalculation(Stock stock, DateTime candleTime, float marginPrice, boolean doPriceScaling) {
+        dateTimeIDList.clear();
+
+        float open = stock.getOpen(candleTime);
+        float close = stock.getClose(candleTime);
+        float min = stock.getMin(candleTime);
+        float max = stock.getMax(candleTime);
+        float topWick = getTopWickSize(max,open,close);
+        float bottomWick = getBottomWickSize(min,open,close);
+
+        int nextGreen = 0;
+        int nextRed = 0;
+
+        //Get similar candles from all data
+        ArrayList<Long> similarCandlesList = new ArrayList<>();
+        int similarCandleCount = 0;
+        for (Map.Entry<Long, String[]> data : stock.getHistoricalData().entrySet()) { //Loops through all stock data
+            float dataOpen = Float.parseFloat(data.getValue()[0]);
+            float dataClose = Float.parseFloat(data.getValue()[1]);
+            float dataMin = Float.parseFloat(data.getValue()[2]);
+            float dataMax = Float.parseFloat(data.getValue()[3]);
+
+            //This value scales based on the total cap of the stock over history so similarities have a chance
+            float pricingScale = doPriceScaling ? dataOpen / open : 1;
+
+            boolean bodySize = isWithinMargin(open,dataOpen / pricingScale,marginPrice) && isWithinMargin(close,dataClose / pricingScale,marginPrice);//isWithinMargin(ocDiff,getOpenCloseDifference(dataOpen,dataClose) / pricingScale,marginPrice);
+            boolean topWickSize = isWithinMargin(topWick, getTopWickSize(dataMax,dataOpen,dataClose) / pricingScale,marginPrice);
+            boolean bottomWickSize = isWithinMargin(bottomWick, getBottomWickSize(dataMin,dataOpen,dataClose) / pricingScale,marginPrice);
+
+            if (bodySize && topWickSize && bottomWickSize) {
+                similarCandlesList.add(data.getKey());
+                long id = data.getKey();
+                DateTime thisTime = new DateTime(id);
+                if (stock.getClose(thisTime.getAdded(60)) > stock.getOpen(thisTime.getAdded(60))) {
+                    nextGreen++;
+                }
+                if (stock.getClose(thisTime.getAdded(60)) < stock.getOpen(thisTime.getAdded(60))) {
+                    nextRed++;
+                }
+                similarCandleCount++;
+            }
+        }
+
+        dateTimeIDList.addAll(similarCandlesList);
+
+        /*
+        int nextGreen = 0;
+        int nextRed = 0;
+
+        //Sort out candles that the previous is unsimilar
+        ArrayList<Long> secondSimilarCandlesList = new ArrayList<>();
+        for (long id : similarCandlesList) {
+            if (isPrevCandleSimilar(stock, candleTime, new DateTime(id), 1, doPriceScaling, marginPrice)) {
+                secondSimilarCandlesList.add(id);
+            }
+        }
+
+        //Sort out candles that the previous-previous is unsimilar
+        for (long id : secondSimilarCandlesList) {
+            if (isPrevCandleSimilar(stock, candleTime, new DateTime(id), 2, doPriceScaling, marginPrice)) {
+                dateTimeIDList.add(id);
+                DateTime thisTime = new DateTime(id);
+                if (stock.getClose(thisTime.getAdded(60)) > stock.getOpen(thisTime.getAdded(60))) {
+                    nextGreen++;
+                }
+                if (stock.getClose(thisTime.getAdded(60)) < stock.getOpen(thisTime.getAdded(60))) {
+                    nextRed++;
+                }
+                similarCandleCount++;
+            }
+        }
+        */
+
+
+        text.setText(similarCandleCount + "\\nGreen Probability: " + MathE.roundDouble((double) nextGreen /similarCandleCount * 100,1) + "%\\nRed Probability: " + MathE.roundDouble((double) nextRed /similarCandleCount * 100,1) + "%");
+    }
+
+    private boolean isPrevCandleSimilar(Stock stock, DateTime mainCandleTime, DateTime similarCandleTime, int candlesBack, boolean doPriceScaling, float marginPrice) {
+        float prevOpen = stock.getOpen(mainCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevClose = stock.getClose(mainCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevMin = stock.getMin(mainCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevMax = stock.getMax(mainCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+
+        float prevTopWick = getTopWickSize(prevMax, prevOpen, prevClose);
+        float prevBottomWick = getBottomWickSize(prevMin, prevOpen, prevClose);
+
+        float prevDataOpen = stock.getOpen(similarCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevDataClose = stock.getClose(similarCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevDataMin = stock.getMin(similarCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+        float prevDataMax = stock.getMax(similarCandleTime.getAdded(-stock.getTimeFrame().getSeconds() * candlesBack));
+
+        float pricingScale = doPriceScaling ? prevDataOpen / prevOpen : 1;
+
+        boolean bodySize = isWithinMargin(prevOpen, prevDataOpen / pricingScale, marginPrice) && isWithinMargin(prevClose, prevDataClose / pricingScale, marginPrice);//isWithinMargin(ocDiff,getOpenCloseDifference(dataOpen,dataClose) / pricingScale,marginPrice);
+        boolean topWickSize = isWithinMargin(prevTopWick, getTopWickSize(prevDataMax, prevDataOpen, prevDataClose) / pricingScale, marginPrice);
+        boolean bottomWickSize = isWithinMargin(prevBottomWick, getBottomWickSize(prevDataMin, prevDataOpen, prevDataClose) / pricingScale, marginPrice);
+
+        return (bodySize && topWickSize && bottomWickSize);
     }
 
     private float getOpenCloseDifference(float open, float close) {
@@ -146,6 +218,10 @@ public class ProbabilityScene extends Scene {
     private boolean isWithinError(float trueValue, float experimentalValue, double maxPercentError) {
         double percentDiff = Math.abs((experimentalValue - trueValue) / trueValue);
         return percentDiff < maxPercentError;
+    }
+
+    private boolean isWithinMargin(float mainValue, float testValue, float margin) {
+        return (mainValue + margin >= testValue && mainValue - margin <= testValue);
     }
 
     @Override
